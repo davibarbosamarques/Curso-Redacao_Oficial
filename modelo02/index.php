cc75810b4f9fc4b8a84c33b0decff3f757e761d6
Davi <dbarbosamarques100@gmail.com>
2023-11-07 16:03:44 -0300

    comecei o modulo02

---
+++ modelo02/index.php
@@ -0,0 +1,89 @@
+<?php
+include('layout/header.php');
+ ?>
+<body>
+<!-- ola -->
+    <!-- NAV  -->
+    <?php
+    include('layout/nav.php');
+    ?>
+    <!-- FIM DA NAV -->
+
+    <!-- menu lateral  -->
+    <?php
+    include('layout/menu.php');
+    ?>
+
+    <div class="all-conteudo">
+        <section>
+            <div class="main-content container-fluid">
+
+
+
+                <div class="row">
+                    <!-- Begin row -->
+                    <div class="col-md-1"></div>
+                    <div class="col-md-10 col-sm-8 container-fluid">
+
+                        <h2 class="Titulo fw-bolder">Apresentação</h2>
+                        <!-- tirei o hr por que o menu não estava ficando sobreposto -->
+                        <!-- <hr> -->
+
+
+                        <p>As relações de trabalho e a comunicação, em qualquer instituição, exigem atenção especial com a forma escrita da língua e seu registro adequado, a fim de que haja o entendimento comum. Os funcionários públicos expedem, então, mensagens para trocar informações, reconhecer direitos e vantagens, estabelecer obrigações, comunicar intenções, realizar negócios.
+                        </p>
+
+                        <p>Desse modo, você deve acompanhar nesta aula as explicações sobre a maneira de diagramar e elaborar as comunicações e os documentos oficiais que estudaremos: ofício, memorando oficial, carta, carta circular, e-mail, pauta de reunião, ata, atestado, declaração e certidão.</p>
+
+                        <p>Bons estudos!</p>
+
+                        <h2 class="Titulo fw-bolder">Objetivos</h2>
+
+                        <!-- lista  -->
+
+                        <ul class="list">
+                            <li class="img-fluid wow fadeInLeft" data-wow-delay="0.3s"><i class="fa fa-bullseye text-success"></i>
+                            Apresentar os aspectos formais de diagramação e formatação de omunicações e documentos oficiais;
+                            </li>
+                            <li class="img-fluid wow fadeInLeft" data-wow-delay="0.3s"><i class="fa fa-bullseye text-success"></i>
+                            Desenvolver habilidades para identificação e resolução de roblemas comunicacionais no âmbito da redação oficial; e
+                            </li>
+                            <li class="img-fluid wow fadeInLeft" data-wow-delay="0.3s"><i class="fa fa-bullseye text-success"></i>
+                            Apontar para competências, que possibilitem a elaboração dequada dos diversos tipos de textos oficiais.
+                            </li>
+                        </ul>
+
+                        <!-- fim da lista  -->
+
+
+                        <ul class="none">
+                            <li class="img-fluid wow fadeInLeft" data-wow-delay="0.3s"><i></i>
+                                <h4>
+                                    Olá, Nome_Completo bem-vindo ao módulo 01 do curso de Língua Portuguesa: Redação Oficial. Bons estudos!
+                                </h4>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
+
+
+        <!-- Botões de navegação -->
+        <div class="text-center">
+            <a href="topico01.php" class="btn btn-outline-success" role="button" aria-pressed="true">Próximo <i
+                    class="bi bi-chevron-right"></i></a>
+        </div>
+
+
+        <div id="progress-bar"></div>
+    </div>
+    
+<?php
+include('layout/footer.php');
+?>
+</body>
+
+</html>
---
+++ modelo02/layout/menu.php
@@ -0,0 +1,99 @@
+
+<nav class="menulateral shadow-lg">
+    <div class="btn-expandir">
+       <a href="https://www.egp.ce.gov.br/" target="_blank"><img src="imgs/logoEGP2.png" alt="logo" id="logo"></a>
+    </div>
+    <!-- os topicos começam aqui  -->
+    <ul>
+        <!-- ADICIONEI NOVOS ÍCONES NOS SUBTÓPICOS -->
+        <li class="menu">
+            <a href="index.php">
+                <span class="icon "><i class="bi bi-person-workspace"></i></span>
+                <span class="txt">Apresentação</span>
+            </a>
+        </li>
+
+         <!-- topico com Subtópico -->
+         <button class="dropdown-btn">
+            <li class="menu">
+                <a data-valor="topico01.php">
+                    <span class="icon space"><i class="bi bi-1-circle"></i></span>
+                    <span class="txt">Comunicação Oficial I</span>
+                        <i class="bi bi-caret-down-fill barra-sub"></i>
+                </a>
+            </li>
+        </button>
+        <!-- subtopicos aqui  -->
+        <div class="dropdown-container">
+            <a href="topico01.php">
+                <i class="bi bi-journals circulo"></i>Início</a>
+            <a href="#item1">
+                <i class="bi bi-journals circulo"></i>1.2 Memorando Oficial</a>
+        </div>
+
+        <!-- topico com Subtópico -->
+        <button class="dropdown-btn">
+            <li class="menu">
+                <a data-valor="topico02.php">
+                    <span class="icon space"><i class="bi bi-2-circle"></i></span>
+                    <span class="txt">Comunicação Oficial II</span>
+                        <i class="bi bi-caret-down-fill barra-sub"></i>
+                </a>
+            </li>
+        </button>
+        <!-- subtopicos aqui  -->
+        <div class="dropdown-container">
+            <a href="topico02.php">
+                <i class="bi bi-journals circulo"></i>Início</a>
+            <a href="#item1">
+                <i class="bi bi-journals circulo"></i>2.2 Modelo de Carta</a>
+            <a href="#item1">
+                <i class="bi bi-journals circulo"></i>2.3 Mensagem Eletrônica</a>
+        </div>
+
+        <!-- topico com Subtópico -->
+        <button class="dropdown-btn">
+            <li class="menu">
+                <a data-valor="topico03.php">
+                    <span class="icon space"><i class="bi bi-3-circle"></i></span>
+                    <span class="txt">Comunicação Oficial III</span>
+                        <i class="bi bi-caret-down-fill barra-sub"></i>
+                </a>
+            </li>
+        </button>
+        <!-- subtopicos aqui  -->
+        <div class="dropdown-container">
+            <a href="topico03.php">
+                <i class="bi bi-journals circulo"></i>Início</a>
+            <a href="#item1">
+                <i class="bi bi-journals circulo"></i>2.1 Características da Redação Oficial</a>
+        </div>
+                <!-- topico com Subtópico -->
+        <button class="dropdown-btn">
+            <li class="menu">
+                <a data-valor="topico04.php">
+                    <span class="icon space"><i class="bi bi-4-circle"></i></span>
+                    <span class="txt">Comunicação Oficial IV</span>
+                        <i class="bi bi-caret-down-fill barra-sub"></i>
+                </a>
+            </li>
+        </button>
+        <!-- subtopicos aqui  -->
+        <div class="dropdown-container">
+            <a href="topico04.php">
+                <i class="bi bi-journals circulo"></i>Início</a>
+            <a href="#item1">
+                <i class="bi bi-journals circulo"></i>2.1 Características da Redação Oficial</a>
+        </div>
+
+        <li class="menu">
+            <a href="referencias.php">
+                <span class="icon"><i class="bi bi-book-half"></i></span>
+                <span class="txt">Referências</span>
+            </a>
+        </li>
+
+    </ul>
+</nav>
+
+</nav>
